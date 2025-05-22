c235652028be3cc3608a883e20663eb1c8daeb89
Piyush Solanki <piyushsolanki18dec@gmail.com>
2025-05-22 16:23:05 +0530

    base_structure

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Retro_site Activation"
+def home():
+    return render_template("home.html")
+
+@app.route('/gym')
+def gym():
+    return render_template("gym.html")
+
+@app.route('/athletes')
+def athletes():
+    return render_template("athletes.html")
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
